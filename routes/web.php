1a15a5a96c1e850518fe7a15b6e7665fc2a7829a
John Doe <johndoe@example.com>
2024-11-16 12:09:44 +0300

    lab 1 & 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Authenticate
+Route::get('/auth/signup', [AuthController::class, 'signup']);
+Route::post('/auth/registr', [AuthController::class, 'registr']);
+
+
+
+
+Route::get('/', [MainController::class, 'index']);
+Route::get('galery/{img}/{name}', function($img, $name){
+    return view('main.galery', ['img'=>$img, 'name'=>$name]);
 });
+
+// Route::get('/', function () {
+//         return view('layout');
+//     });
+
+Route::get('/about', function(){
+    return view('main.about');
+});
+
+Route::get('/contacts', function(){
+    $data = [
+        'city'=>'Moscow',
+        'street'=>'Semenovskaya',
+        'house'=>38,
+    ];
+    return view('main.contact', ['data'=>$data]);
+});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
